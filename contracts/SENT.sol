d85cdbbee361605ab4a8268890867ff2e2c6f41c
Sean Darcy <darcys22@gmail.com>
2024-04-03 14:28:31 +1100

    SENT token

---
+++ contracts/SENT.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./libraries/Shared.sol";
+
+/**
+ * @title SENT contract
+ * @notice The SENT utility token
+ */
+contract SENT is ERC20, Shared {
+    constructor(
+        uint256 totalSupply,
+        address receiverGenesisAddress
+    )
+        ERC20("Session", "SENT")
+        nzAddr(receiverGenesisAddress)
+        nzUint(totalSupply)
+    {
+        _mint(receiverGenesisAddress, totalSupply);
+    }
+
+    function decimals() public pure override returns (uint8) {
+        return 9;
+    }
+}

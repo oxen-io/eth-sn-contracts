e2e921349e1ed2c38160cb8a6acfb1c10f6f689b
Sean Darcy <darcys22@gmail.com>
2024-04-04 16:41:53 +1100

    tests for RewardRatePool

--- contracts/RewardRatePool.sol
+++ contracts/RewardRatePool.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
+
 contract RewardRatePool {
     using SafeERC20 for IERC20;
 
@@ -12,8 +13,8 @@ contract RewardRatePool {
     address immutable public beneficiary;
     uint256 public totalPaidOut;
     uint256 public lastPaidOutTime;
-    uint256 public constant ANNUAL_INTEREST_RATE = 145; // 14.5% in tenths of a percent
-    uint256 public constant BASIS_POINTS = 1000; // Basis points for percentage calculation
+    uint64 public constant ANNUAL_INTEREST_RATE = 145; // 14.5% in tenths of a percent
+    uint64 public constant BASIS_POINTS = 1000; // Basis points for percentage calculation
 
     constructor(address _beneficiary, address _sent) {
         beneficiary = _beneficiary;
@@ -21,26 +22,46 @@ contract RewardRatePool {
         SENT = IERC20(_sent);
     }
 
+    // EVENTS
+    event FundsReleased(uint256 amount);
+
+    //////////////////////////////////////////////////////////////
+    //                                                          //
+    //                  State-changing functions                //
+    //                                                          //
+    //////////////////////////////////////////////////////////////
+
+    // Calculate and pay out interest, then update the timestamp and total paid out
+    function payoutReleased() public {
+        uint256 released = calculateReleasedAmount(block.timestamp);
+        totalPaidOut += released;
+        SENT.safeTransfer(beneficiary, released);
+        emit FundsReleased(released);
+    }
+
+    //////////////////////////////////////////////////////////////
+    //                                                          //
+    //                Non-state-changing functions              //
+    //                                                          //
+    //////////////////////////////////////////////////////////////
+
+    function rewardRate(uint256 timestamp) public view returns (uint256) {
+        uint256 alreadyReleased = calculateReleasedAmount(timestamp);
+        uint256 totalDeposited = calculateTotalDeposited();
+        return calculateInterestAmount(totalDeposited - alreadyReleased, 2 minutes);
+    }
+
     function calculateTotalDeposited() public view returns (uint256) {
         return SENT.balanceOf(address(this)) + totalPaidOut;
     }
 
-    // Calculate and pay out interest, then update the timestamp and total paid out
-    function payoutReleased() public {
-        require(block.timestamp > lastPaidOutTime, "Cannot pay out yet.");
-        uint256 totalDeposited = calculateTotalDeposited();
-        uint256 timeElapsed = block.timestamp - lastPaidOutTime;
-        lastPaidOutTime = block.timestamp;
-        uint256 released = (totalDeposited - totalPaidOut) * ANNUAL_INTEREST_RATE * timeElapsed / (BASIS_POINTS * 365 days);
-        totalPaidOut += released;
-        SENT.safeTransfer(beneficiary, released);
+    function calculateReleasedAmount(uint256 timestamp) public view returns (uint256) {
+        uint256 timeElapsed = timestamp - lastPaidOutTime;
+        return totalPaidOut + calculateInterestAmount(SENT.balanceOf(address(this)), timeElapsed);
     }
 
-    function rewardRate(uint256 timestamp) public view returns (uint256) {
-        uint256 totalDeposited = calculateTotalDeposited();
-        uint256 timeElapsed = timestamp - lastPaidOutTime;
-        uint256 alreadyReleased = totalPaidOut + (totalDeposited - totalPaidOut) * ANNUAL_INTEREST_RATE * timeElapsed / (BASIS_POINTS * 365 days);
-        return (totalDeposited - alreadyReleased) * ANNUAL_INTEREST_RATE * 2 minutes / (BASIS_POINTS * 365 days);
+    function calculateInterestAmount(uint256 balance, uint256 timeElapsed) public pure returns (uint256) {
+        return balance * ANNUAL_INTEREST_RATE * timeElapsed / (BASIS_POINTS * 365 days);
     }
 }
 
--- contracts/test/MockERC20.sol
+++ contracts/test/MockERC20.sol
@@ -4,8 +4,13 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockERC20 is ERC20 {
-    constructor(string memory name, string memory symbol, uint8 decimals) ERC20(name, symbol) {
-        _mint(msg.sender, 1000000 * (10 ** uint256(decimals))); // Mint 1 million tokens for testing
+    uint8 d;
+    constructor(string memory name, string memory symbol, uint8 _decimals) ERC20(name, symbol) {
+        d = _decimals;
+        _mint(msg.sender, 1000000 * (10 ** uint256(d))); // Mint 1 million tokens for testing
+    }
+    function decimals() public view override returns (uint8) {
+        return d;
     }
 }
 

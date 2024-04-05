a35962f447ee5a140b3e98ef7a67d129b7251f3f
Sean Darcy <darcys22@gmail.com>
2024-04-05 11:27:15 +1100

    docs for RewardRatePool

--- contracts/RewardRatePool.sol
+++ contracts/RewardRatePool.sol
@@ -3,7 +3,10 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-
+/**
+ * @title Reward Rate Pool Contract
+ * @dev Implements reward distribution based on a fixed annual interest rate.
+ */
 contract RewardRatePool {
     using SafeERC20 for IERC20;
 
@@ -15,7 +18,12 @@ contract RewardRatePool {
     uint256 public lastPaidOutTime;
     uint64 public constant ANNUAL_INTEREST_RATE = 145; // 14.5% in tenths of a percent
     uint64 public constant BASIS_POINTS = 1000; // Basis points for percentage calculation
-
+    
+    /**
+     * @dev Sets the initial beneficiary and SENT token address.
+     * @param _beneficiary Address that will receive the interest payouts.
+     * @param _sent Address of the SENT ERC20 token contract.
+     */
     constructor(address _beneficiary, address _sent) {
         beneficiary = _beneficiary;
         lastPaidOutTime = block.timestamp;
@@ -31,7 +39,10 @@ contract RewardRatePool {
     //                                                          //
     //////////////////////////////////////////////////////////////
 
-    // Calculate and pay out interest, then update the timestamp and total paid out
+    /**
+     * @dev Calculates and releases the due interest payout to the beneficiary.
+     * Updates the total paid out and the last payout time.
+     */    
     function payoutReleased() public {
         uint256 released = calculateReleasedAmount(block.timestamp);
         totalPaidOut += released;
@@ -45,21 +56,41 @@ contract RewardRatePool {
     //                                                          //
     //////////////////////////////////////////////////////////////
 
+    /**
+     * @dev Returns the block reward for a 2 minutes block at a certain timestamp.
+     * @param timestamp The timestamp of the block.
+     * @return The calculated block reward.
+     */
     function rewardRate(uint256 timestamp) public view returns (uint256) {
         uint256 alreadyReleased = calculateReleasedAmount(timestamp);
         uint256 totalDeposited = calculateTotalDeposited();
         return calculateInterestAmount(totalDeposited - alreadyReleased, 2 minutes);
     }
-
+    
+    /**
+     * @dev Calculates the total amount of SENT tokens deposited in the contract.
+     * @return The sum of SENT tokens currently held by the contract and the total amount previously paid out.
+     */
     function calculateTotalDeposited() public view returns (uint256) {
         return SENT.balanceOf(address(this)) + totalPaidOut;
     }
 
+    /**
+     * @dev Calculates the amount of SENT tokens released up to a specific timestamp.
+     * @param timestamp The timestamp until which to calculate the released amount.
+     * @return The calculated amount of SENT tokens released.
+     */
     function calculateReleasedAmount(uint256 timestamp) public view returns (uint256) {
         uint256 timeElapsed = timestamp - lastPaidOutTime;
         return totalPaidOut + calculateInterestAmount(SENT.balanceOf(address(this)), timeElapsed);
     }
 
+    /**
+     * @dev Calculates 14.5% annual interest for a given balance and time period.
+     * @param balance The principal balance to calculate interest on.
+     * @param timeElapsed The time period over which to calculate interest.
+     * @return The calculated interest amount.
+     */
     function calculateInterestAmount(uint256 balance, uint256 timeElapsed) public pure returns (uint256) {
         return balance * ANNUAL_INTEREST_RATE * timeElapsed / (BASIS_POINTS * 365 days);
     }

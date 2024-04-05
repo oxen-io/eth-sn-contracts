32e61cf5189032f396ee8f02c4c9fcb4158be03b
Sean Darcy <darcys22@gmail.com>
2024-04-05 11:30:02 +1100

    remove unused shared functions

--- contracts/libraries/Shared.sol
+++ contracts/libraries/Shared.sol
@@ -2,15 +2,13 @@
 
 pragma solidity ^0.8.0;
 
-import "../interfaces/IShared.sol";
-
 /**
  * @title    Shared contract
  * @notice   Holds constants and modifiers that are used in multiple contracts
  * @dev      It would be nice if this could be a library, but modifiers can't be exported :(
  */
 
-abstract contract Shared is IShared {
+abstract contract Shared {
     /// @dev The address used to indicate whether transfer should send native or a token
     address internal constant _NATIVE_ADDR = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
     address internal constant _ZERO_ADDR = address(0);
@@ -32,12 +30,6 @@ abstract contract Shared is IShared {
     /// @dev    Checks that a bytes32 isn't zero/empty
     modifier nzBytes32(bytes32 b) {
         require(b != _NULL, "Shared: bytes32 input is empty");
-        _;
-    }
-
-    /// @dev    Checks that the pubKeyX is populated
-    modifier nzKey(Key memory key) {
-        require(key.pubKeyX != 0, "Shared: pubKeyX is empty");
         _;
     }
 }

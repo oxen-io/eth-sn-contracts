f1ff112d03ec1855898bf7158eb5d09c92e801b5
doylet <doyle.github.2ifxi@simplelogin.com>
2024-05-15 13:49:31 +1000

    Simplify vesting w/staking by deferring contributors to rewards contract

--- contracts/utils/TokenVestingStaking.sol
+++ contracts/utils/TokenVestingStaking.sol
@@ -89,13 +89,20 @@ contract TokenVestingStaking is ITokenVestingStaking, Shared {
      * @param blsSignature - 128 byte signature
      * @param serviceNodeParams - Service node public key, signature proving ownership of public key and fee that operator is charging
      */
-    function addBLSPublicKey(BN256G1.G1Point calldata blsPubkey, IServiceNodeRewards.BLSSignatureParams calldata blsSignature, IServiceNodeRewards.ServiceNodeParams calldata serviceNodeParams) external onlyBeneficiary notRevoked afterStart {
+    function addBLSPublicKey(BN256G1.G1Point calldata blsPubkey,
+                             IServiceNodeRewards.BLSSignatureParams calldata blsSignature,
+                             IServiceNodeRewards.ServiceNodeParams calldata serviceNodeParams) external
+        onlyBeneficiary
+        notRevoked
+        afterStart
+    {
         uint256 stakingRequirement = stakingRewardsContract.stakingRequirement();
-        uint64 serviceNodeID = stakingRewardsContract.nextServiceNodeID();
+        uint64 serviceNodeID       = stakingRewardsContract.nextServiceNodeID();
         SENT.approve(address(stakingRewardsContract), stakingRequirement);
         investorServiceNodes.push(ServiceNode(serviceNodeID, stakingRequirement));
-        IServiceNodeRewards.Contributor[] memory contributors = new IServiceNodeRewards.Contributor[](1);
-        contributors[0] = IServiceNodeRewards.Contributor(address(this), stakingRequirement);
+
+        // NOTE: Pass empty array, the contract will assume sender (this contract) as operator.
+        IServiceNodeRewards.Contributor[] memory contributors = new IServiceNodeRewards.Contributor[](0);
         stakingRewardsContract.addBLSPublicKey(blsPubkey, blsSignature, serviceNodeParams, contributors);
     }
 

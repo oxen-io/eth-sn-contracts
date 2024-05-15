b786d6a3e109baa326737c983977b03bbb1939aa
doylet <doyle.github.2ifxi@simplelogin.com>
2024-05-15 15:01:50 +1000

    Suppress warnings about too-many-digits in test code via slither

--- contracts/test/MockERC20.sol
+++ contracts/test/MockERC20.sol
@@ -7,7 +7,7 @@ contract MockERC20 is ERC20 {
     uint8 immutable d;
     constructor(string memory name_, string memory symbol_, uint8 _decimals) ERC20(name_, symbol_) {
         d = _decimals;
-        _mint(msg.sender, 100000000 * (10 ** uint256(d))); // Mint 100 million tokens for testing
+        _mint(msg.sender, 1e8 * (10 ** uint256(d))); // Mint 100 million tokens for testing
     }
     function decimals() public view override returns (uint8) {
         return d;

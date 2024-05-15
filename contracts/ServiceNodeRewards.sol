c953fe09a9a3524a7d8d0438b9567e9dfefca868
doylet <doyle.github.2ifxi@simplelogin.com>
2024-05-15 14:09:48 +1000

    Remove unused strings import in SN rewards via slither

--- contracts/ServiceNodeRewards.sol
+++ contracts/ServiceNodeRewards.sol
@@ -6,7 +6,6 @@ import "./interfaces/IServiceNodeRewards.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
 import "./libraries/Pairing.sol";
 

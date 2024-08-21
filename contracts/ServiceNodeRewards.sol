bd2613db9bf5885fb1e3ed50bdf020b8fdd9ba7b
doylet <doyle.github.2ifxi@simplelogin.com>
2024-08-21 17:04:42 +1000

    Move admin contract to testnet contract to prep for new testnet

--- contracts/ServiceNodeRewards.sol
+++ contracts/ServiceNodeRewards.sol
@@ -778,16 +778,6 @@ contract ServiceNodeRewards is Initializable, Ownable2StepUpgradeable, PausableU
         emit BLSNonSignerThresholdMaxUpdated(newMax);
     }
 
-    // NOTE: Admin function to remove node by ID for stagenet debugging
-    function removeNodeBySNID(uint64[] calldata ids) external onlyOwner {
-        for (uint256 i = 0; i < ids.length; i++) {
-            uint64 serviceNodeID = ids[i];
-            IServiceNodeRewards.ServiceNode memory node = this.serviceNodes(serviceNodeID);
-            require(node.operator != address(0));
-            _removeBLSPublicKey(serviceNodeID, node.deposit);
-        }
-    }
-
     //////////////////////////////////////////////////////////////
     //                                                          //
     //                Non-state-changing functions              //

ba0f52296e55ee2b69f338cd18626ddf11d3868f
doylet <doyle.github.2ifxi@simplelogin.com>
2024-08-21 17:04:42 +1000

    Move admin contract to testnet contract to prep for new testnet

--- contracts/test/TestnetServiceNodeRewards.sol
+++ contracts/test/TestnetServiceNodeRewards.sol
@@ -4,9 +4,13 @@ pragma solidity ^0.8.20;
 import "../ServiceNodeRewards.sol";
 
 contract TestnetServiceNodeRewards is ServiceNodeRewards {
-    // New function for owner to remove nodes without BLS signature validation
-    function removeNodeByOwner(uint64 serviceNodeID) external onlyOwner {
-        IServiceNodeRewards.ServiceNode memory node = this.serviceNodes(serviceNodeID);
-        _removeBLSPublicKey(serviceNodeID, node.deposit);
+    // NOTE: Admin function to remove node by ID for stagenet debugging
+    function removeNodeBySNID(uint64[] calldata ids) external onlyOwner {
+        for (uint256 i = 0; i < ids.length; i++) {
+            uint64 serviceNodeID = ids[i];
+            IServiceNodeRewards.ServiceNode memory node = this.serviceNodes(serviceNodeID);
+            require(node.operator != address(0));
+            _removeBLSPublicKey(serviceNodeID, node.deposit);
+        }
     }
 }

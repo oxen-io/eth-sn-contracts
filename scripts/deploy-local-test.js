8395e766e23f6c1872068346a7374990867372ea
Sean Darcy <darcys22@gmail.com>
2024-09-06 11:27:33 +1000

    enfore a minimum liquidator ratio

--- scripts/deploy-local-test.js
+++ scripts/deploy-local-test.js
@@ -35,7 +35,7 @@ async function main() {
         await rewardRatePool.getAddress(),         // foundation pool address
         120_000_000_000,                           // staking requirement
         10,                                        // max contributors
-        0,                                         // liquidator reward ratio
+        1,                                         // liquidator reward ratio
         0,                                         // pool share of liquidation ratio
         1                                          // recipient ratio
     ]);

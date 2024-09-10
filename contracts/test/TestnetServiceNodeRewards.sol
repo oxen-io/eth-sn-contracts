2f996e88ff7926945c1b952880e58706465d8491
doylet <doyle.github.2ifxi@simplelogin.com>
2024-09-10 12:06:52 +1000

    Add request remove node by SNID admin function for stagenet

--- contracts/test/TestnetServiceNodeRewards.sol
+++ contracts/test/TestnetServiceNodeRewards.sol
@@ -5,6 +5,15 @@ import "../ServiceNodeRewards.sol";
 
 contract TestnetServiceNodeRewards is ServiceNodeRewards {
     // NOTE: Admin function to remove node by ID for stagenet debugging
+    function requestRemoveNodeBySNID(uint64[] calldata ids) external onlyOwner {
+        for (uint256 i = 0; i < ids.length; i++) {
+            uint64 serviceNodeID = ids[i];
+            IServiceNodeRewards.ServiceNode memory node = this.serviceNodes(serviceNodeID);
+            require(node.operator != address(0));
+            _initiateRemoveBLSPublicKey(serviceNodeID, node.operator);
+        }
+    }
+
     function removeNodeBySNID(uint64[] calldata ids) external onlyOwner {
         for (uint256 i = 0; i < ids.length; i++) {
             uint64 serviceNodeID = ids[i];

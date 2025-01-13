1c95fabe5384b4471a3bd820adc6ef36c755c314
Sean Darcy <darcys22@gmail.com>
2025-01-13 11:30:55 +1100

    l1 deployed address

--- scripts/deploy-stagenet.js
+++ scripts/deploy-stagenet.js
@@ -3,7 +3,7 @@ require("./testnet-common.js")();
 
 async function main() {
     // NOTE: We reuse the token address of the existing $SESH contract on stagenet
-    const TOKEN_ADDRESS = "0x70c1f36C9cEBCa51B9344121D284D85BE36CD6bB";
+    const TOKEN_ADDRESS = "0x7D7fD4E91834A96cD9Fb2369E7f4EB72383bbdEd";
 
     const args = {
       TOKEN_ADDRESS
